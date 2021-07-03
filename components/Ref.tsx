7222c4b7049cff11b7a664b783357f7723a2b153
Nimo Beeren <nimobeeren@gmail.com>
2021-07-03 14:10:52 +0200

    add simple ref component with placeholder counter

---
+++ components/Ref.tsx
@@ -0,0 +1,30 @@
+function mapNodeNameToPrefix(nodeName: string) {
+  if (nodeName == 'H2') {
+    return 'Section'
+  }
+  if (['H3', 'H4', 'H5', 'H6'].includes(nodeName)) {
+    return 'Subsection'
+  }
+  return null;
+}
+
+export function Ref({ to }) {
+  const toElement = document.getElementById(to)
+
+  if (!toElement) {
+    return <a>??</a>
+  }
+
+  console.log(toElement)
+  const prefix = mapNodeNameToPrefix(toElement.nodeName)
+  const counter = "2.3" // TODO: may not be possible with CSS counters
+
+  let text: string
+  if (prefix) {
+    text = `${prefix} ${counter}`
+  } else {
+    text = counter
+  }
+
+  return <a href={`#${to}`}>{text}</a>
+}
