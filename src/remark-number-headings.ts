8ab6e7b9d2affb2d222026592a489314d6e2102e
Nimo Beeren <nimobeeren@gmail.com>
2021-07-27 13:33:44 +0200

    allow setting which heading depths are numbered

--- src/remark-number-headings.ts
+++ src/remark-number-headings.ts
@@ -1,6 +1,11 @@
 import { visit } from 'unist-util-visit'
 
-function attacher() {
+function attacher(config: any = {}) {
+  const { 
+    // Range of heading depths which should be numbered (inclusive)
+    headingDepth = [2, 6]
+  } = config
+
   return transformer
 
   function transformer(tree, file) {
@@ -10,7 +15,7 @@ function attacher() {
       3: 0,
       4: 0,
       5: 0,
-      6: 0,
+      6: 0
     }
 
     visit(tree, 'heading', visitor)
@@ -18,16 +23,24 @@ function attacher() {
     function visitor(node) {
       counter[node.depth]++
 
-      let prefix = String(counter[1])
-      for (let depth = 2; depth <= node.depth; depth++) {
-        prefix += `.${counter[depth]}`
-      }
+      const [minDepth, maxDepth] = headingDepth
 
-      node.children.unshift({
-        type: 'text',
-        // Prefix and heading title are separated by an 'em quad' character
-        value: `${prefix}\u2001`
-      })
+      if (minDepth <= node.depth && node.depth <= maxDepth) {
+        let prefix = String(counter[minDepth])
+        for (
+          let depth = minDepth + 1;
+          depth <= node.depth && depth <= maxDepth;
+          depth++
+        ) {
+          prefix += `.${counter[depth]}`
+        }
+
+        node.children.unshift({
+          type: 'text',
+          // Prefix and heading title are separated by an 'em quad' character
+          value: `${prefix}\u2001`
+        })
+      }
     }
   }
 }
