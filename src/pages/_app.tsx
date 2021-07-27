2bb786b53cbb1a5f6127c2c5a6b6d9ebb65bf51d
Nimo Beeren <nimobeeren@gmail.com>
2021-07-27 23:47:41 +0200

    remove old way of numbering headings

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,19 +2,6 @@ import { MDXProvider } from '@mdx-js/react'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import '../../styles/globals.css'
-import { Heading } from '../components/Heading'
-import { HeadingProvider } from '../context/HeadingContext'
-
-const components = {
-  /* eslint-disable react/display-name */
-  h1: (props) => <Heading depth={1} {...props} />,
-  h2: (props) => <Heading depth={2} {...props} />,
-  h3: (props) => <Heading depth={3} {...props} />,
-  h4: (props) => <Heading depth={4} {...props} />,
-  h5: (props) => <Heading depth={5} {...props} />,
-  h6: (props) => <Heading depth={6} {...props} />
-  /* eslint-enable react/display-name */
-}
 
 function App({ Component, pageProps }: AppProps) {
   return (
@@ -27,11 +14,9 @@ function App({ Component, pageProps }: AppProps) {
           crossOrigin="anonymous"
         />
       </Head>
-      <HeadingProvider>
-        <MDXProvider components={components}>
-          <Component {...pageProps} />
-        </MDXProvider>
-      </HeadingProvider>
+      <MDXProvider>
+        <Component {...pageProps} />
+      </MDXProvider>
     </>
   )
 }
