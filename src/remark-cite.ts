f1031cab5c6f4b9e040ef47b9fda65c603805e8f
Nimo Beeren <nimobeeren@gmail.com>
2021-08-03 21:21:18 +0200

    parse bibtex

--- src/remark-cite.ts
+++ src/remark-cite.ts
@@ -1,23 +1,34 @@
+import fetch from 'cross-fetch'
 import { Text } from 'mdast'
 import { Plugin } from 'unified'
 import { u } from 'unist-builder'
 import { visit } from 'unist-util-visit'
+import { Bibliography } from './pages/api/bibliography'
 
-const MOCK_BIBLIOGRAPHY = [
-  {
-    id: 'newton',
-    value: 'I. Newton, Philosophiae naturalis principia mathematica.'
-  },
-  { id: 'yolo', value: 'J. Redmon, YOLOv3.' },
-  { id: 'thinking', value: 'D. Kahneman, Thinking Fast and Slow' }
-]
+const MOCK_BIBLIOGRAPHY = `@article{dijkstra1959,
+  title={A note on two problems in connexion with graphs},
+  author={Dijkstra, Edsger W and others},
+  journal={Numerische mathematik},
+  volume={1},
+  number={1},
+  pages={269--271},
+  year={1959}
+}
+
+@book{walker2017,
+  title={Why we sleep: Unlocking the power of sleep and dreams},
+  author={Walker, Matthew},
+  year={2017},
+  publisher={Simon and Schuster}
+}
+`
 
 function getBibItemHtmlId(num: number) {
   return `bibitem-${num}`
 }
 
 const attacher: Plugin<[]> = () => {
-  return (tree) => {
+  return async (tree) => {
     const citations: any[] = []
 
     // Find all citation nodes
@@ -34,26 +45,40 @@ const attacher: Plugin<[]> = () => {
       }
     })
 
-    const filteredBibliography = MOCK_BIBLIOGRAPHY
+    const parsedBibliography: Bibliography = await fetch('/api/bibliography', {
+      method: 'POST',
+      body: MOCK_BIBLIOGRAPHY
+    }).then((res) => res.json())
+
+    const filteredBibliography = parsedBibliography
       // Keep only bibitems that were actually referenced
-      .filter((bibItem) =>
-        citations.some((citation) => citation.id === bibItem.id)
+      .filter((bibEntry) =>
+        citations.some((citation) => citation.id === bibEntry.id)
       )
       // Sort alphabetically
-      .sort((a, b) => (a.value > b.value ? 1 : -1))
+      .sort((a, b) => (a.author[0].family > b.author[0].family ? 1 : -1))
 
-    // Create a References section at the end of the document
+    // Create a bibliography section at the end of the document
     if (filteredBibliography.length > 0) {
       // Add References heading
       ;(tree.children as any).push(
         u('heading', { depth: 2 }, [u('text', 'References')])
       )
+
       // Add a list of references
       ;(tree.children as any).push(
         u(
           'list',
-          filteredBibliography.map((bibItem, index) =>
-            u(
+          filteredBibliography.map((bibEntry, index) => {
+            const authorString = [
+              bibEntry.author[0].given,
+              bibEntry.author[0].family
+            ]
+              .filter(Boolean)
+              .join(' ')
+            const bibEntryString = `${authorString}. ${bibEntry.title}`
+
+            return u(
               'listItem',
               {
                 data: {
@@ -61,9 +86,9 @@ const attacher: Plugin<[]> = () => {
                   hProperties: { id: getBibItemHtmlId(index + 1) }
                 }
               },
-              [u('paragraph', [u('text', `[${index + 1}] ${bibItem.value}`)])]
+              [u('paragraph', [u('text', `[${index + 1}] ${bibEntryString}`)])]
             )
-          )
+          })
         )
       )
     }
