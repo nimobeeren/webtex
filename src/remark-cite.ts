4987756af35620cad31d18d859315b9068f67ff9
Nimo Beeren <nimobeeren@gmail.com>
2021-08-06 19:45:37 +0200

    create class to store all citation engine stuff

--- src/remark-cite.ts
+++ src/remark-cite.ts
@@ -1,4 +1,4 @@
-import { BibLatexParser, CSLExporter, CSLOutput } from 'biblatex-csl-converter'
+import { BibLatexParser, CSLEntry, CSLExporter } from 'biblatex-csl-converter'
 import CiteProc from 'citeproc'
 import fetch from 'cross-fetch'
 import { Text } from 'mdast'
@@ -40,73 +40,179 @@ const MOCK_BIBLIOGRAPHY = `@article{dijkstra1959,
 }
 `
 
-const CITATION_STYLE = 'apa'
-// const CITATION_STYLE = 'acm-sig-proceedings'
+/** Reference to a specific bibliographic resource */
+type CitationItem = string
+/** Cluster of citation items referenced in the same place */
+type Citation = CitationItem[]
+/** Data structure returned by citeproc when formatting a citation */
+type CiteProcCitationTriple = [
+  string, // ID used internally by citeproc
+  number, // nodeIndex
+  string // formatted citation
+]
 
-function parseBibtex(bibtex: string) {
-  // Parse BibLaTeX string to internal structure
-  const parser = new BibLatexParser(bibtex)
-  const { entries: internalStructure, errors, warnings } = parser.parse()
-  for (let error of errors || []) {
-    console.error(error)
+/**
+ * Stateful structure that produces a bibliography and citations, formatted in a
+ * context-aware manner, so as to adhere to a particular citation style.
+ */
+class CitationEngine {
+  bibliography: Record<string, CSLEntry> = {}
+  citations: Citation[] = []
+  style = 'apa'
+  locale = 'en-US'
+
+  citeProc: any
+  citeProcState: CiteProcCitationTriple[] = []
+
+  constructor(style?: string, locale?: string) {
+    if (style != null) this.style = style
+    if (locale != null) this.locale = locale
   }
-  for (let warning of warnings || []) {
-    console.warn(warning)
+
+  /**
+   * Must be called (and awaited!) before using other instance methods.
+   */
+  async init() {
+    this.citeProc = await this.createCiteProc()
   }
 
-  // Convert internal structure to CSL
-  const exporter = new CSLExporter(parser.bibDB)
-  const csl = exporter.parse()
-  // When converting to CSL, the original BibTeX key is replaced by an internal
-  // key, which is not useful to us. To preserve the BibTeX key, we get it from
-  // the internal structure and store it in the CSL. The internal key should be
-  // the same in both structures.
-  for (let [internalKey, cslEntry] of Object.entries(csl)) {
-    cslEntry.id = internalStructure[internalKey].entry_key
-  }
+  /**
+   * Creates an instance of the CiteProc CSL engine.
+   * @returns the instance
+   */
+  async createCiteProc() {
+    const self = this
 
-  return csl
-}
+    const citationLocale = await fetch(
+      `/citations/locales/locales-${this.locale}.xml`
+    ).then((res) => res.text())
 
-function getCslItem(csl: CSLOutput, id: string) {
-  return Object.values(csl).find((item) => item.id === id)
-}
+    const citationStyle = await fetch(
+      `/citations/styles/${this.style}.csl`
+    ).then((res) => res.text())
 
-async function createCiteproc(csl: CSLOutput) {
-  // Fetch the en-US locale, since it's the only one currently supported
-  const locale = await fetch('/citations/locales/locales-en-US.xml').then(
-    (res) => res.text()
-  )
+    return new CiteProc.Engine(
+      {
+        retrieveLocale(lang: string) {
+          if (lang === self.locale) {
+            return citationLocale
+          } else {
+            return null
+          }
+        },
 
-  const style = await fetch(`/citations/styles/${CITATION_STYLE}.csl`).then(
-    (res) => res.text()
-  )
-
-  return new CiteProc.Engine(
-    {
-      retrieveLocale(lang: string) {
-        if (lang === 'en-US') {
-          return locale
-        } else {
-          return null
+        retrieveItem(id: string) {
+          const entry = self.getBibEntry(id)
+          if (!entry) {
+            throw new Error(
+              `Tried to retrieve non-existent bibliography entry with ID: ${id}`
+            )
+          }
+          return entry
         }
       },
+      citationStyle
+    )
+  }
 
-      retrieveItem(id: string) {
-        const item = getCslItem(csl, id)
-        if (!item) {
-          throw new Error(
-            `Tried to retrieve non-existent citation item with ID: ${id}`
-          )
-        }
-        return item
+  /**
+   * Sets the bibliography, which contains the items that can be cited.
+   * This method must be called before setting citations.
+   * @param bibtex string containing the bibliography in BibTeX format
+   */
+  setBibliography(bibtex: string) {
+    this.bibliography = CitationEngine.parseBibtex(bibtex)
+  }
+
+  /**
+   * Sets all citations in the document, replacing the old citations if there
+   * were any.
+   */
+  setCitations(citations: Citation[]) {
+    this.citations = citations
+
+    const citeProcCitations = citations.map((citation) => {
+      if (citation.length === 0) {
+        throw new Error("Can't register citation without item IDs")
       }
-    },
-    style
-  )
+      return {
+        citationItems: citation.map((item) => ({ id: item })),
+        properties: {
+          nodeIndex: 0 // for in-text citations rather than footnotes
+        }
+      }
+    })
+    this.citeProcState = this.citeProc.rebuildProcessorState(
+      citeProcCitations,
+      'html'
+    )
+  }
+
+  /**
+   * Gets an entry from the bibliography if it exists.
+   * @returns the entry in CSL-JSON format, or `undefined` if it doesn't exist
+   */
+  getBibEntry(id: string) {
+    if (!this.bibliography) {
+      return undefined
+    }
+    return Object.values(this.bibliography).find((entry) => entry.id === id)
+  }
+
+  /**
+   * Generates a formatted bibliography.
+   * @returns a HTML string of the bibliography
+   */
+  getFormattedBibliography(): string {
+    const bib = this.citeProc.makeBibliography()
+    return [bib[0].bibstart, ...bib[1], bib[0].bibend].join('')
+  }
+
+  /**
+   * Generates formatted citations.
+   * @returns an array of HTML strings, one for each citation
+   */
+  getFormattedCitations(): string[] {
+    // Only return the formatted citation, the rest is not useful for us
+    return this.citeProcState.map((triple) => triple[2])
+  }
+
+  /**
+   * Parses a bibliography in BibTeX format and returns it in CSL-JSON format.
+   * @returns CSL-JSON representation of the bibliography
+   */
+  static parseBibtex(bibtex: string) {
+    // Parse BibLaTeX string to internal structure
+    const parser = new BibLatexParser(bibtex)
+    const { entries: internalStructure, errors, warnings } = parser.parse()
+    for (let error of errors || []) {
+      console.error(error)
+    }
+    for (let warning of warnings || []) {
+      console.warn(warning)
+    }
+
+    // Convert internal structure to CSL
+    const exporter = new CSLExporter(parser.bibDB)
+    const csl = exporter.parse()
+    // When converting to CSL, the original BibTeX key is replaced by an internal
+    // key, which is not useful to us. To preserve the BibTeX key, we get it from
+    // the internal structure and store it in the CSL. The internal key should be
+    // the same in both structures.
+    for (let [internalKey, cslEntry] of Object.entries(csl)) {
+      cslEntry.id = internalStructure[internalKey].entry_key
+    }
+
+    return csl
+  }
 }
 
-function getCitationItemIds(citation: Node) {
+/**
+ * Gets citation item IDs from a given node, assuming that it is a citation
+ * node.
+ * @returns the citation, which could contain multiple citation items
+ */
+function getCitationFromNode(citation: Node): Citation {
   // Get the citation ID, given by the joined values of all descendant
   // text nodes
   let citationId = ''
@@ -118,66 +224,33 @@ function getCitationItemIds(citation: Node) {
 }
 
 /**
- * Registers citations and returns a formatted HTML string for each one. Only
- * citations that contain one or more citation item IDs are supported.
- * @returns An array of the same length as `citations`, containing HTML strings
- * corresponding to the given citations.
+ * Remark plugin to generate formatted citations.
  */
-function registerCitations(citeproc, citations: Node[]): string[] {
-  const citeprocCitations = citations.map((citation) => {
-    const ids = getCitationItemIds(citation)
-    if (ids.length === 0) {
-      throw new Error("Can't register citation without item IDs")
-    }
-    return {
-      citationItems: ids.map((id) => ({ id })),
-      properties: {
-        nodeIndex: 0 // for in-text citations rather than footnotes
-      }
-    }
-  })
-
-  type CitationTriple = [
-    string, // ID used internally by citeproc
-    number, // nodeIndex
-    string // formatted citation
-  ]
-  const triples: CitationTriple[] = citeproc.rebuildProcessorState(
-    citeprocCitations,
-    'html'
-  )
-
-  // Only return the formatted citation, the rest is not useful for us
-  return triples.map((result) => result[2])
-}
-
-/**
- * Generates a HTML string for a bibliography.
- */
-function makeBibliography(citeproc): string {
-  const bib = citeproc.makeBibliography()
-  return [bib[0].bibstart, ...bib[1], bib[0].bibend].join('')
-}
-
 const attacher: Plugin<[]> = () => {
   return async (tree) => {
-    const csl = parseBibtex(MOCK_BIBLIOGRAPHY)
-    const citeproc = await createCiteproc(csl)
+    const engine = new CitationEngine()
+    await engine.init()
+    engine.setBibliography(MOCK_BIBLIOGRAPHY)
 
     // Find all citation nodes
-    let citations: Node[] = []
+    const citations: Citation[] = []
+    const citationNodeMap = new Map<Citation, Node>()
     visit(tree, 'textDirective', (node) => {
       if (node.name === 'cite') {
-        citations.push(node)
+        const citation = getCitationFromNode(node)
+        citations.push(citation)
+        citationNodeMap.set(citation, node)
       }
     })
 
-    let knownCitations: Node[] = [] // valid and exists in the bibliography
-    let unknownCitations: Node[] = [] // valid, but does not exist
+    // Check if citations refer to existing bibliography entries
+    let knownCitations: Citation[] = []
+    let unknownCitations: Citation[] = []
     for (let citation of citations) {
-      const ids = getCitationItemIds(citation)
-      // Check if citation items exist in the bibliography
-      if (ids.length > 0 && ids.every((id) => getCslItem(csl, id))) {
+      if (
+        citation.length > 0 &&
+        citation.every((item) => engine.getBibEntry(item))
+      ) {
         knownCitations.push(citation)
       } else {
         // Citation refers to at least one non-existant item
@@ -185,29 +258,36 @@ const attacher: Plugin<[]> = () => {
       }
     }
 
-    const formattedCitations = registerCitations(citeproc, knownCitations)
-    const formattedBibliography = makeBibliography(citeproc)
+    engine.setCitations(knownCitations)
+    const formattedCitations = engine.getFormattedCitations()
 
-    // Replace each valid citation node with a formatted citation string
+    // Replace each known citation node with a formatted citation string
     for (let i = 0; i < knownCitations.length; i++) {
-      knownCitations[i].type = 'html'
-      knownCitations[i].value = formattedCitations[i]
+      const node = citationNodeMap.get(knownCitations[i])
+      node!.type = 'html'
+      node!.value = formattedCitations[i]
     }
 
-    for (let unknownCitation of unknownCitations) {
-      const ids = getCitationItemIds(unknownCitation)
-      unknownCitation.type = 'strong'
-      unknownCitation.children = [
-        u('text', ids.length > 0 ? `:cite[${ids.join(',')}]` : ':cite')
+    // Replace unknown citation nodes with a warning string
+    for (let citation of unknownCitations) {
+      const node = citationNodeMap.get(citation)
+      node!.type = 'strong'
+      node!.children = [
+        u(
+          'text',
+          citation.length > 0 ? `:cite[${citation.join(',')}]` : ':cite'
+        )
       ]
     }
 
-    // Create a bibliography section at the end of the document
-    ;(tree.children as any).push(
-      u('heading', { depth: 2 }, [u('text', 'References')])
-    )
-    // Add the formatted bibliography as HTML
-    ;(tree.children as any).push(u('html', formattedBibliography))
+    if (knownCitations.length > 0) {
+      // Create a bibliography section at the end of the document
+      ;(tree.children as any).push(
+        u('heading', { depth: 2 }, [u('text', 'References')])
+      )
+      // Add the formatted bibliography as HTML
+      ;(tree.children as any).push(u('html', engine.getFormattedBibliography()))
+    }
   }
 }
 
