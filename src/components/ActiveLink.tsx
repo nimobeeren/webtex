1b62f308db66151fdf2acaf912ac682cb172af61
Nimo Beeren <nimobeeren@gmail.com>
2021-11-24 22:44:39 +0100

    wip: continue work on introduction page

--- src/components/ActiveLink.tsx
+++ src/components/ActiveLink.tsx
@@ -2,6 +2,8 @@ import { Link, LinkProps } from "@chakra-ui/layout";
 import { useRouter } from "next/router";
 import React from 'react'
 
+// FIXME: link is not active when # is appended to the URL
+
 export const ActiveLink = React.forwardRef<HTMLAnchorElement, LinkProps>(function ActiveLink({ href, ...restProps }, ref) {
   const { asPath } = useRouter();
 
