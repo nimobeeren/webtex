fb658d08e2ab565c9a18a438650455cf43e3eff7
Nimo Beeren <nimobeeren@gmail.com>
2021-12-01 22:26:30 +0100

    fix link not showing as active

--- src/components/ActiveLink.tsx
+++ src/components/ActiveLink.tsx
@@ -1,18 +1,21 @@
 import { Link, LinkProps } from "@chakra-ui/layout";
 import { useRouter } from "next/router";
-import React from 'react'
+import React from "react";
 
-// FIXME: link is not active when # is appended to the URL
+export const ActiveLink = React.forwardRef<HTMLAnchorElement, LinkProps>(
+  function ActiveLink({ href, ...restProps }, ref) {
+    const { asPath } = useRouter();
 
-export const ActiveLink = React.forwardRef<HTMLAnchorElement, LinkProps>(function ActiveLink({ href, ...restProps }, ref) {
-  const { asPath } = useRouter();
+    // Need to pass a base URL to make it a valid URL
+    const { pathname } = new URL(asPath, "https://some.site/");
 
-  return (
-    <Link
-      ref={ref}
-      href={href}
-      aria-current={asPath === href ? "page" : undefined}
-      {...restProps}
-    />
-  );
-})
+    return (
+      <Link
+        ref={ref}
+        href={href}
+        aria-current={pathname === href ? "page" : undefined}
+        {...restProps}
+      />
+    );
+  }
+);
