54686179eff90707e97de764076d66f45ae0ef15
Nimo Beeren <nimobeeren@gmail.com>
2022-01-24 17:09:50 +0100

    fix trpc request failing on client

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -24,24 +24,25 @@ function App({ Component, pageProps }: AppProps) {
 
 export default withTRPC<AppRouter>({
   config({ ctx }) {
-    /**
-     * If you want to use SSR, you need to use the server's full URL
-     * @link https://trpc.io/docs/ssr
-     */
+    if (process.browser) {
+      return {
+        url: "/api/trpc"
+      };
+    }
+
+    // During SSR below
+
     const url = process.env.VERCEL_URL
       ? `https://${process.env.VERCEL_URL}/api/trpc`
       : "http://localhost:3000/api/trpc";
 
     return {
-      url
-      /**
-       * @link https://react-query.tanstack.com/reference/QueryClient
-       */
-      // queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
+      url,
+      headers: {
+        // optional - inform server that it's an ssr request
+        "x-ssr": "1"
+      }
     };
   },
-  /**
-   * @link https://trpc.io/docs/ssr
-   */
   ssr: true
 })(App);
