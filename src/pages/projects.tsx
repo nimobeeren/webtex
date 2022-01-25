546312418d7c00472b994c4e3465cbe6b044f6fd
Nimo Beeren <nimobeeren@gmail.com>
2022-01-26 00:57:49 +0100

    formatting

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -30,66 +30,58 @@ function ProjectsPage() {
     return <Spinner />;
   }
   return (
-      <Flex direction="column" height="100%">
+    <Flex direction="column" height="100%">
       <Head>
         <title>WebTeX | Projects</title>
       </Head>
-        <Header flexShrink={0}>
-          <NextLink href="/" passHref>
-            <Logo as="a" ml={6} />
-          </NextLink>
-          <Spacer />
-          <NextLink href="/docs" passHref>
-            <DocsButton as="a" />
-          </NextLink>
-          <FeedbackButton variant="ghost" />
-          <NextLink href="https://github.com/nimobeeren/webtex" passHref>
-            <GitHubButton as="a" />
-          </NextLink>
-        </Header>
-        <Box
-          as="main"
-          flexGrow={1}
-          w="100%"
-          maxW={960}
-          m="0 auto"
-          px={8}
-          pt={16}
-        >
-          <Heading as="h1" mb={8}>
-            Projects
-          </Heading>
-          {projects.data.length == 0 ? (
-            <VStack spacing={8} align="flex-start">
-              <Text>{"Looks like you haven't saved any projects yet."}</Text>
-              <NextLink href="/" passHref>
-                <Button as="a">Start writing!</Button>
-              </NextLink>
-            </VStack>
-          ) : (
-            <Table size="lg">
-              <Thead>
-                <Tr>
-                  <Th>Title</Th>
-                  <Th>Last changed</Th>
+      <Header flexShrink={0}>
+        <NextLink href="/" passHref>
+          <Logo as="a" ml={6} />
+        </NextLink>
+        <Spacer />
+        <NextLink href="/docs" passHref>
+          <DocsButton as="a" />
+        </NextLink>
+        <FeedbackButton variant="ghost" />
+        <NextLink href="https://github.com/nimobeeren/webtex" passHref>
+          <GitHubButton as="a" />
+        </NextLink>
+      </Header>
+      <Box as="main" flexGrow={1} w="100%" maxW={960} m="0 auto" px={8} pt={16}>
+        <Heading as="h1" mb={8}>
+          Projects
+        </Heading>
+        {projects.data.length == 0 ? (
+          <VStack spacing={8} align="flex-start">
+            <Text>{"Looks like you haven't saved any projects yet."}</Text>
+            <NextLink href="/" passHref>
+              <Button as="a">Start writing!</Button>
+            </NextLink>
+          </VStack>
+        ) : (
+          <Table size="lg">
+            <Thead>
+              <Tr>
+                <Th>Title</Th>
+                <Th>Last changed</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {projects.data.map((project) => (
+                <Tr key={project.id} _hover={{ bg: "gray.50" }}>
+                  <Td>
+                    <NextLink href={`/project/${project.id}`} passHref>
+                      <Link>{project.title || "Untitled"}</Link>
+                    </NextLink>
+                  </Td>
+                  <Td>Unknown</Td>
                 </Tr>
-              </Thead>
-              <Tbody>
-                {projects.data.map((project) => (
-                  <Tr key={project.id} _hover={{ bg: "gray.50" }}>
-                    <Td>
-                      <NextLink href={`/project/${project.id}`} passHref>
-                        <Link>{project.title || "Untitled"}</Link>
-                      </NextLink>
-                    </Td>
-                    <Td>Unknown</Td>
-                  </Tr>
-                ))}
-              </Tbody>
-            </Table>
-          )}
-        </Box>
-      </Flex>
+              ))}
+            </Tbody>
+          </Table>
+        )}
+      </Box>
+    </Flex>
   );
 }
 
